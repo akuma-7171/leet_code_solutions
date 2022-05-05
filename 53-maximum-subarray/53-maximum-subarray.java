1298d70fec9880d9bca596b4ea219a722c20d138
akuma-7171 <104818160+akuma-7171@users.noreply.github.com>
2022-05-05 23:33:39 +0530

    Time: 2 ms (56.25%), Space: 74.3 MB (32.65%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int max = nums[0];
+        
+        int curr = max;
+        
+        
+        for(int i=1; i<nums.length; i++){
+            curr = Math.max(nums[i]+curr, nums[i]);
+            max = Math.max(max, curr);
+        }
+        
+        
+        return max;
+        
+    }
+}
