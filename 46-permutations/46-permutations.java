186d44d8faf6788dfbd1d0d690f1891347ecc5f6
akuma-7171 <104818160+akuma-7171@users.noreply.github.com>
2022-05-05 23:16:29 +0530

    Time: 1 ms (93.41%), Space: 44.8 MB (37.19%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,29 +1,27 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        
+   public List<List<Integer>> permute(int[] letters) {
         List<List<Integer>> res = new ArrayList<>();
-        
-        permutation(res, new ArrayList<>(), nums);
-        
+        dfs(new ArrayList<>(), new boolean[letters.length], res, letters);
         return res;
     }
-    
-    public void permutation(List<List<Integer>> res, List<Integer> curr, int []nums){
-        
-        if(curr.size() == nums.length){
-            res.add(new ArrayList<>(curr));
+
+    private static void dfs(List<Integer> path, boolean[] used, List<List<Integer>> res, int[] letters) {
+        if (path.size() == used.length) {
+            
+            res.add(new ArrayList<Integer>(path));
             return;
         }
-        
-        for(int i=0; i<nums.length; i++){
+
+        for (int i = 0; i < used.length; i++) {
+           
+            if (used[i]) continue;
             
-            if(!curr.contains(nums[i])){
-                curr.add(nums[i]);
-                permutation(res,curr,nums);
-                curr.remove(curr.size()-1);
-            }
+            path.add(letters[i]);
+            used[i] = true;
+            dfs(path, used, res, letters);
             
+            path.remove(path.size() - 1);
+            used[i] = false;
         }
-        
-    }
+    }    
 }
