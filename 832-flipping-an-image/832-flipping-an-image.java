1ca4a0923ce1dee1d5acec64887eb118f4cdf5d4
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-05-10 00:23:35 +0530

    Time: 1 ms (65.16%), Space: 45.2 MB (22.25%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.java
+++ 832-flipping-an-image/832-flipping-an-image.java
@@ -1,8 +1,11 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
+        
+        
          for(int i =0; i< image.length; i++){
+             
             int start =0, end=image[i].length -1;
-			//Flipping
+			
             while(start < end){
                 int temp = image[i][end];
                 image[i][end] = image[i][start];
@@ -10,11 +13,13 @@ class Solution {
                 start++;
                 end--;
             }
-			//Revert
+			
             for(int j =0; j< image[i].length; j++){
                 image[i][j] = (image[i][j] == 0) ? 1 : 0;
             }
         }
         return image;
+        
+        
     }
 }
