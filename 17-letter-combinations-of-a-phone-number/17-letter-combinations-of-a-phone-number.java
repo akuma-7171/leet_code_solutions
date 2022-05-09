639708188b01a99f5194bbfffb2757ae6f6ad670
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-05-09 11:34:04 +0530

    Time: 5 ms (42.13%), Space: 41.2 MB (82.26%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,25 +1,45 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
+    public List<String> letterCombinations(String digits) {
         
-        if(strs == null){
-            return null;
+        List<String> res = new ArrayList<>();
+        
+        if(digits == null || digits.length() == 0){
+            return res;
         }
         
-        String prefix = strs[0];
+        String[] mapping = {
+            
+            "0",
+            "1",
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz"
+            
+        };
+        
+        lettercombination(res, digits, "", 0, mapping);
+        return res;
         
         
-        for(int i=1; i<strs.length; i++){
-            
-            while(strs[i].indexOf(prefix) != 0){
-                
-                prefix = prefix.substring(0, prefix.length()-1);
-                
-            }
-            
+        
+    }
+    
+    public void lettercombination(List<String> res, String digits, String current, int index, String[] mapping){
+        if(index == digits.length()){
+            res.add(current);
+            return;
         }
         
+        String letter = mapping[digits.charAt(index)-'0'];
         
-        return prefix;
+        for(int i=0; i<letter.length(); i++){
+            lettercombination(res, digits, current+letter.charAt(i), index+1, mapping);
+        }
         
     }
 }
