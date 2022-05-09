4c08d95baf3fb47af0c4504ee1982496da1c55f2
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-05-09 15:18:59 +0530

    Time: 2 ms (68.10%), Space: 55.1 MB (26.57%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -4,13 +4,14 @@ class Solution {
         int index = 0;
         
         for(int i=0; i<nums.length; i++){
-           if(nums[i] != 0){
-               nums[index++] = nums[i];
-           } 
+            if(nums[i] != 0){
+                nums[index++] = nums[i];
+            }
         }
         
-        for(int j=index; j<nums.length; j++){
-            nums[j] = 0;
+        for(int i=index; i<nums.length; i++){
+            nums[i] = 0;
         }
+        
     }
 }
