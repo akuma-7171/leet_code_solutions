5f1aecf6538c2097133ffa05405b810b64541ad4
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-05-09 18:28:12 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (47.59%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -3,7 +3,6 @@ class Solution {
         
         int p1 = m-1;
         int p2 = n-1;
-        
         int i = m+n-1;
         
         while(p2 >= 0){
@@ -17,6 +16,7 @@ class Solution {
             }
             
         }
+        
         
     }
 }
