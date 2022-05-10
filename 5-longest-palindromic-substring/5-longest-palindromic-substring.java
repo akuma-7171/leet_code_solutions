80e61de15e2260fa8fd902cb9d058df4ba24ce11
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-05-10 19:03:57 +0530

    Time: 58 ms (49.11%), Space: 43.1 MB (62.42%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,41 +1,27 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        
-        int start = 0;
-        int end = 0;
-        
-        for(int i=0; i<s.length(); i++){
-            
-            int len1 = check(s, i, i);
-            int len2 = check(s, i, i+1);
-            
-            int len3 = Math.max(len1, len2);
-            
-            if(len3 > end-start){
-                
-                start = i - (len3-1)/2;
-                end = i+(len3/2);
-                
-            }
-            
-        }
-        
-        
-        return s.substring(start, end+1);
-        
-    }
     
-    
-    public int check (String s, int left, int right){
-        
-        if(left > right || s == null) return 0;
-        
-        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-            left--;
-            right++;
-        }
-        
-        
-        return right-left-1;
-    }
+                private int lo, maxLen;
+
+                public String longestPalindrome(String s) {
+                    int len = s.length();
+                    if (len < 2)
+                        return s;
+
+                    for (int i = 0; i < len-1; i++) {
+                        extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+                        extendPalindrome(s, i, i+1); //assume even length.
+                    }
+                    return s.substring(lo, lo + maxLen);
+                }
+
+                public void extendPalindrome(String s, int j, int k) {
+                    while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+                        j--;
+                        k++;
+                    }
+                    if (maxLen < k - j - 1) {
+                        lo = j + 1;
+                        maxLen = k - j - 1;
+                    }
+                }
 }
