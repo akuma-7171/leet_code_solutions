793e648b2d79e739cc58526b2eea1fd91074b7b3
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-05-10 23:37:25 +0530

    Time: 1 ms (56.83%), Space: 42.3 MB (42.29%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -1,25 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
         
-        if(strs == null){
-            return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        
+        for(int i=0; i<n; i++){
+            fast = fast.next;
         }
         
-        String prefix = strs[0];
-        
-        
-        for(int i=1; i<strs.length; i++){
-            
-            while(strs[i].indexOf(prefix) != 0){
-                
-                prefix = prefix.substring(0, prefix.length()-1);
-                
-            }
-            
+        if(fast == null){
+            return head.next;
         }
         
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
         
-        return prefix;
+        slow.next = slow.next.next;
+        
+        return head;
         
     }
 }
