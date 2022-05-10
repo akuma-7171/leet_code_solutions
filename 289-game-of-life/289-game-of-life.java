be96a5dddecf19878fe0857717c4f2077812eb57
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-05-10 15:53:02 +0530

    Time: 1 ms (44.76%), Space: 42.4 MB (44.47%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,29 +1,48 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-    int rows = board.length, cols = board[0].length;
-    int[][] tempBoard = new int[rows][cols];                                             // we will first update values in this temporary matrix
-    for(int r = 0; r < rows; r++){
-        for(int c = 0; c < cols; c++){
-            int liveNeighbors = neighbor(board, r-1, c-1) + neighbor(board, r-1, c) + neighbor(board, r-1, c+1) + neighbor(board, r, c+1) + 
-                                neighbor(board, r+1, c+1) + neighbor(board, r+1, c) + neighbor(board, r+1, c-1) + neighbor(board, r, c-1);
-            //System.out.println("cell: ("+r+","+c+") ,  liveNeighbors: "+liveNeighbors);
-            if(board[r][c] == 1){ 
-                tempBoard[r][c] = (liveNeighbors < 2 || liveNeighbors > 3) ? 0 : 1;      // update temporary matrix, based on the rules in question
-            }else{ 
-                tempBoard[r][c] = (liveNeighbors == 3) ? 1 : 0;
+        
+        if(board == null || board.length == 0){
+            return;
+        }
+        
+        int row = board.length;
+        int column = board[0].length;
+        
+        int [][] tempboard = new int[row][column];
+        
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column; j++){
+                
+                int livecount = count(board,i-1,j) + count(board,i+1,j) + count(board,i,j-1) + count(board,i,j+1) + count(board, i-1,j-1) + count(board, i+1,j+1) + count(board, i+1, j-1) + count(board, i-1, j+1);
+                
+                if(board[i][j] == 1){
+                    
+                    tempboard[i][j] = (livecount < 2 || livecount > 3) ? 0 : 1;
+                    
+                }
+                
+                else{
+                    tempboard[i][j] = (livecount == 3) ? 1 : 0;
+                }
+                
+                
             }
         }
+        
+        
+        for(int i=0; i<row; i++){
+            board[i] = tempboard[i].clone();
+        }
+        
+        
     }
     
-    for(int r = 0; r < rows; r++){
-        board[r] = tempBoard[r].clone();                                                // copy all tempBoard elements back to board matrix
+    public int count(int [][]board, int i, int j){
+        
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] == 0) return 0;
+        
+        return 1;
+        
     }
-}
-
-// ----------------------------------------------------------------------- //
-public int neighbor(int[][] board, int r, int c){
-    if( r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] == 0 ){ return 0; }                          // out of bound cases
-    return 1;
-}
-
+    
 }
