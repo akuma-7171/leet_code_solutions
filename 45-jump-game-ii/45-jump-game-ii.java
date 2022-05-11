63a71f5d6043522623593be2d55f26baca79ec80
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-05-11 19:35:47 +0530

    Time: 1 ms (99.89%), Space: 49.2 MB (60.79%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -2,21 +2,24 @@ class Solution {
     public int jump(int[] nums) {
 
         int jump = 0;
-		int left = 0;
-		int right = 0;
-		//till we reach end of the array
-		while (right < nums.length - 1) {
-			int max = 0;
-			//find max reach (in range of indexes left and right)
-			for (int i = left; i <= right; i++) {
-				max = Math.max(nums[i] + i, max);
-			}
-			//update new boundaries
-			left = right + 1;
-			right = max;
-			jump++;
-		}
-		return jump;
-	
+        int left = 0;
+        int right = 0;
+        
+        while(right < nums.length-1){
+            
+            int max = 0;
+            
+            for(int i=left; i<=right; i++){
+                max = Math.max(max, nums[i]+i);
+            }
+            
+            left = right+1;
+            right=max;
+            jump++;
+            
+        }
+        
+        return jump;
+        
     }
 }
