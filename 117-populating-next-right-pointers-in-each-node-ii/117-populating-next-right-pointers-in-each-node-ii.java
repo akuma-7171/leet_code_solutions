0e6e7da6948e42b1ba7c3d7730974372215f8828
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-05-14 11:52:19 +0530

    Time: 2 ms (41.64%), Space: 45.1 MB (34.48%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -1,49 +1,37 @@
 class Solution {
-  public Node connect(Node root) {
-    if (root == null) {
-      return root;
+    public Node connect(Node root) {
+        
+        if(root == null) return root;
+        
+        Queue<Node> q = new LinkedList<>();
+        
+        q.add(root);
+        
+        while(!q.isEmpty()){
+            
+            int size = q.size();
+            
+            Node dummy = new Node(0);
+            
+            while(size-- > 0){
+                Node node = q.remove();
+                
+                dummy.next = node;
+                dummy = dummy.next;
+                
+                if(node.left != null){
+                    q.add(node.left);
+                }
+                
+                if(node.right != null){
+                    q.add(node.right);
+                }
+                
+            }
+            
+        }
+        
+        return root;
+        
     }
-
-    connectNexts(root);
-    connect(root.right);
-    connect(root.left);
-    return root;
-  }
-
-  void connectNexts(Node root) {
-    if (root == null) {
-      return;
-    }
-
-    if (root.right != null) {
-      if (root.next != null) {
-        root.right.next = findNode(root.next);
-      }
-      if (root.left != null) {
-        root.left.next = root.right;
-      }
-    } else {
-      if (root.left != null && root.next != null) {
-        root.left.next = findNode(root.next);
-      }
-    }
-    // connectNexts(root.next);
-  }
-
-  Node findNode(Node root) {
-
-    while (root != null) {
-      if (root.left != null) {
-        return root.left;
-      }
-
-      if (root.right != null) {
-        return root.right;
-      }
-
-      root = root.next;
-    }
-
-    return null;
-  }
 }
