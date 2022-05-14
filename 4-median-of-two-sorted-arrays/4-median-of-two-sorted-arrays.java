3d34342ae288048b1d0ef77755ebe07ef02e8d04
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-05-14 12:20:55 +0530

    Time: 4 ms (58.12%), Space: 50.3 MB (19.32%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,9 +1,9 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
-        int len = nums1.length + nums2.length;
+        int len = nums1.length + nums2.length; // take a length of both the arrays;
         
-        int []merge = new int[len];
+        int []merge = new int[len]; // make new array
         
         int i=0;
         int j=0;
@@ -11,8 +11,8 @@ class Solution {
         
         while(i<nums1.length && j<nums2.length){
             if(nums1[i] < nums2[j]){
-                merge[k++] = nums1[i++]; 
-            }
+                merge[k++] = nums1[i++];  // run the for loop till one of the array exhausted
+            }                             // and fill the new array in ascending order
             
             else{
                 merge[k++] = nums2[j++];
@@ -20,11 +20,11 @@ class Solution {
         }
         
         while(i<nums1.length){
-            merge[k++] = nums1[i++];
+            merge[k++] = nums1[i++]; // if nums1 is still left fill the new array by nums1
         }
         
         while(j<nums2.length){
-            merge[k++] = nums2[j++];
+            merge[k++] = nums2[j++]; // if nums2 is still left fill the new array with nums2
         }
         
         double median = 0.0;
@@ -32,11 +32,11 @@ class Solution {
         int mid = merge.length/2;
         
         if(merge.length%2 == 1){
-            median = merge[mid];
+            median = merge[mid]; // if array length is odd return mid element
         }
         
         else{
-            median = (merge[mid]+merge[mid-1])/2.0;
+            median = (merge[mid]+merge[mid-1])/2.0; // else mid element + mid-1 element
         }
         return median;
     }
