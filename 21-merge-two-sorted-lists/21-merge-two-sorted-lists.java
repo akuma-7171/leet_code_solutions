9dbf7cf0dd3ef89227019b38405e3cdda367847a
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-05-22 15:41:55 +0530

    Time: 1 ms (65.58%), Space: 43.4 MB (28.24%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -1,25 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         
-        if(strs == null){
-            return null;
-        }
-        
-        String prefix = strs[0];
+        ListNode temp = new ListNode(0);
+        ListNode current = temp;
         
         
-        for(int i=1; i<strs.length; i++){
+        while(l1 != null && l2 != null){
             
-            while(strs[i].indexOf(prefix) != 0){
-                
-                prefix = prefix.substring(0, prefix.length()-1);
-                
+            if(l1.val < l2.val){
+                current.next = l1;
+                l1 = l1.next;
             }
             
+            else{
+                current.next = l2;
+                l2 = l2.next;
+            }
+            
+            current = current.next;
+            
         }
         
         
-        return prefix;
+        if(l1 != null){
+            current.next = l1;
+            l1 = l1.next;
+        }
+        
+        
+        if(l2 != null){
+            current.next = l2;
+            l2 = l2.next;
+        }
+        
+        return temp.next;
         
     }
 }
