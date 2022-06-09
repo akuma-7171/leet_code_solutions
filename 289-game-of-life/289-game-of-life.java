a4de86eaf3a7cb3ae7256c25896d2618064cc2a6
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-09 19:31:35 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (97.59%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -8,41 +8,39 @@ class Solution {
         int row = board.length;
         int column = board[0].length;
         
-        int [][] tempboard = new int[row][column];
+        int [][]temp = new int[row][column];
         
         for(int i=0; i<row; i++){
             for(int j=0; j<column; j++){
                 
-                int livecount = count(board,i-1,j) + count(board,i+1,j) + count(board,i,j-1) + count(board,i,j+1) + count(board, i-1,j-1) + count(board, i+1,j+1) + count(board, i+1, j-1) + count(board, i-1, j+1);
+                int livecount = count(board,i+1, j) + count(board, i-1,j) + count(board, i,j-1) + count(board, i, j+1) + count(board,i+1, j+1) + count(board, i-1, j-1) + count(board, i+1, j-1) + count(board, i-1, j+1);
                 
                 if(board[i][j] == 1){
-                    
-                    tempboard[i][j] = (livecount < 2 || livecount > 3) ? 0 : 1;
-                    
+                    temp[i][j] = (livecount < 2 || livecount > 3) ? 0:1;
                 }
                 
                 else{
-                    tempboard[i][j] = (livecount == 3) ? 1 : 0;
+                    temp[i][j] = (livecount == 3) ? 1:0;
                 }
-                
                 
             }
         }
         
         
         for(int i=0; i<row; i++){
-            board[i] = tempboard[i].clone();
+            board[i] = temp[i].clone();
         }
-        
         
     }
     
+    
     public int count(int [][]board, int i, int j){
         
-        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] == 0) return 0;
+        
+        if(i<0 || i>=board.length || j<0 || j>= board[0].length || board[i][j] == 0) return 0;
+        
         
         return 1;
-        
     }
     
 }
