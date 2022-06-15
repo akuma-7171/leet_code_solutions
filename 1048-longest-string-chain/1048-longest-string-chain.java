eaa1776a13002d643fecea75544e95d379f3bf63
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-15 10:21:31 +0530

    Time: 69 ms (44.53%), Space: 71.6 MB (14.00%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -5,7 +5,7 @@ class Solution {
         int res = 0;
         for (String word : words) {
             int best = 0;
-            for (int i = 0; i < word.length(); ++i) {
+            for (int i = 0; i < word.length(); i++) {
                 String prev = word.substring(0, i) + word.substring(i + 1);
                 best = Math.max(best, dp.getOrDefault(prev, 0) + 1);
             }
