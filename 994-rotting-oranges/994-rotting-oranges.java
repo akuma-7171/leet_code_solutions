66143bbb8863b9c057c9f9905ec8e77ca2728281
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-16 17:27:56 +0530

    Time: 24 ms (5.12%), Space: 45.4 MB (5.13%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        
+        HashSet<String> fresh = new HashSet<>();
+        HashSet<String> rotten = new HashSet<>();
+        
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j] == 1){
+                    fresh.add(""+i+j);
+                }
+                
+                else if(grid[i][j] == 2){
+                    rotten.add(""+i+j);
+                }
+            }
+        }
+        
+        
+        int min = 0;
+        int[][] dir = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+        
+        while(fresh.size() > 0){
+            HashSet<String> infected = new HashSet<>();
+            
+            for(String s : rotten){
+                int i = s.charAt(0)-'0';
+                int j = s.charAt(1)-'0';
+                
+                
+                for(int[] d : dir){
+                    int next_i = i+d[0];
+                    int next_j = j+d[1];
+                    
+                    if(fresh.contains(""+next_i+next_j)){
+                        fresh.remove(""+next_i+next_j);
+                        infected.add(""+next_i+next_j);
+                    }
+                }
+            }
+            
+            if(infected.size() == 0){
+                return -1;
+            }
+            
+            rotten = infected;
+            min++;
+            
+        }
+        return min;
+    }
+}
