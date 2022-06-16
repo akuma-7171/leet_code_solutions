798bdf9001854a2f96753c16e019df18f42ed3e7
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-16 20:17:13 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (13.18%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -3,19 +3,17 @@ class Solution {
         
         int start = 0;
         int end = nums.length-1;
-        
         int index = 0;
         
-        while(index <= end && start < end){
+        while(index<=end && start<end){
             
             if(nums[index] == 0){
                 nums[index] = nums[start];
                 nums[start] = 0;
-                start++;
-                index++;
+                start++;index++;
             }
             
-            else if(nums[index] == 2){
+           else if(nums[index] == 2){
                 nums[index] = nums[end];
                 nums[end] = 2;
                 end--;
@@ -26,7 +24,6 @@ class Solution {
             }
             
         }
-        
         
     }
 }
