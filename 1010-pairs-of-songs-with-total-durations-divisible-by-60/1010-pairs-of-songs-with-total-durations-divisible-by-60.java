84b39509214df994497476994cc393ceccedb066
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-17 13:13:55 +0530

    Time: 2 ms (99.91%), Space: 49.4 MB (88.47%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,13 +1,13 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
         
-        int []c = new int[60];
+        int []rem = new int[60];
         int res = 0;
         
         for(int t : time){
             
-            res += c[(600-t)%60];
-            c[t%60] += 1;
+            res += rem[(600-t)%60];
+            rem[t%60] += 1;
             
         }
         
