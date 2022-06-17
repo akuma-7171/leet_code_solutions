0c2321a26a78498bf3f40ee7a445d729072ee130
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-17 11:11:17 +0530

    Time: 22 ms (5.07%), Space: 45.8 MB (5.10%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -5,30 +5,34 @@ class Solution {
         HashSet<String> rotten = new HashSet<>();
         
         for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j] == 1){
-                    fresh.add(""+i+j);                  // find location for both
-                }
-                
-                else if(grid[i][j] == 2){
-                    rotten.add(""+i+j);
-                }
-            }
+
+           for(int j=0; j<grid[0].length; j++){
+               
+               if(grid[i][j] == 1){
+                   fresh.add(""+i+j);       // find the location for both
+               }
+               
+               else if(grid[i][j] == 2){
+                   rotten.add(""+i+j);
+               }
+           }
         }
         
-        
         int minutes = 0;
-        int[][] dir = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+        
+        int[][] dir = {{0,-1},{-1,0},{1,0},{0,1}};
         
         while(fresh.size() > 0){
+            
             HashSet<String> infected = new HashSet<>();
             
             for(String s : rotten){
+                
                 int i = s.charAt(0)-'0';
                 int j = s.charAt(1)-'0';
                 
-                
-                for(int[] d : dir){
+                for(int[] d: dir){
+                    
                     int next_i = i+d[0];
                     int next_j = j+d[1];
                     
@@ -36,7 +40,9 @@ class Solution {
                         fresh.remove(""+next_i+next_j);
                         infected.add(""+next_i+next_j);
                     }
+                    
                 }
+                
             }
             
             if(infected.size() == 0){
@@ -47,6 +53,8 @@ class Solution {
             minutes++;
             
         }
+        
         return minutes;
+        
     }
 }
