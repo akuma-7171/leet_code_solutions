e607f94529fa9034f289e1f91f405f62a4ddf9a9
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-17 12:06:53 +0530

    Time: 2 ms (75.00%), Space: 55.4 MB (5.69%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -4,14 +4,13 @@ class Solution {
         int index = 0;
         
         for(int i=0; i<nums.length; i++){
-            
             if(nums[i] != 0){
                 nums[index++] = nums[i];
             }
         }
         
         for(int i=index; i<nums.length; i++){
-            nums[i] = 0;  
+            nums[i] = 0;
         }
         
     }
