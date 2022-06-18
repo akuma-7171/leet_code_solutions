2310b8ded7b597ec35953ccdec3e881776ab35f9
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-18 17:50:29 +0530

    Time: 6 ms (67.02%), Space: 42.6 MB (43.35%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,22 +1,32 @@
 class Solution {
+    
     int count = 0;
     
     public int countSubstrings(String s) {
-        if (s == null || s.length() == 0) return 0;
+        if(s == null || s.length() == 0){
+            return 0;
+        }
         
-        for (int i = 0; i < s.length(); i++) { // i is the mid point
-            extendPalindrome(s, i, i); // odd length;
-            extendPalindrome(s, i, i + 1); // even length
+        for(int i=0; i<s.length(); i++){
+            
+           expand(s,i,i);
+           expand(s,i,i+1);
+            
+        }
+        
+        return count;  
+    }
+    
+    public int expand(String s, int left, int right){
+        
+        while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
+            count++;
+            right++;
+            left--;
         }
         
         return count;
+        
     }
     
-    private void extendPalindrome(String s, int left, int right) {
-        while (left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            count++;
-            left--;
-            right++;
-        }
-    }
 }
