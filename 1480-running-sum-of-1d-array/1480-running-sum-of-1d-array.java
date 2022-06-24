b2ae81e345fda90555b7cc52b292386c9edfe432
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-24 16:45:33 +0530

    Time: 1 ms (16.93%), Space: 43.4 MB (43.77%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -5,7 +5,7 @@ class Solution {
         nums[1] = nums[0]+nums[1];
         
         for(int i=2; i<nums.length; i++){
-            nums[i] = nums[i-1]+nums[i];
+            nums[i] = nums[i]+nums[i-1];
         }
         
         return nums;
