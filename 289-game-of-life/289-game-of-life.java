c755c4e02874b6cb6c97daabf3c17b6caefe64e8
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-25 17:34:05 +0530

    Time: 1 ms (57.36%), Space: 42.3 MB (57.08%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,31 +1,24 @@
 class Solution {
     public void gameOfLife(int[][] board) {
         
-        if(board == null || board.length == 0){
-            return;
-        }
-        
         int row = board.length;
         int column = board[0].length;
         
-        int [][]temp = new int[row][column];
+        int[][] temp = new int[row][column];
         
-        for(int i=0; i<row; i++){
-            for(int j=0; j<column; j++){
-                
-                int livecount = count(board,i+1, j) + count(board, i-1,j) + count(board, i,j-1) + count(board, i, j+1) + count(board,i+1, j+1) + count(board, i-1, j-1) + count(board, i+1, j-1) + count(board, i-1, j+1);
-                
-                if(board[i][j] == 1){
-                    temp[i][j] = (livecount < 2 || livecount > 3) ? 0:1;
-                }
-                
-                else{
-                    temp[i][j] = (livecount == 3) ? 1:0;
-                }
-                
-            }
+       for(int i=0; i<row; i++){
+           for(int j=0; j<column; j++){
+                int livecount = count(board,i+1,j) + count(board,i-1,j) + count(board,i,j+1) + count(board,i,j-1) + count(board,i+1,j+1) + count(board,i-1,j-1) + count(board,i+1,j-1) + count(board,i-1,j+1);
+        
+        if(board[i][j] == 1){
+            temp[i][j] = (livecount<2 || livecount>3) ? 0:1;
         }
         
+        else{
+            temp[i][j] = (livecount == 3) ? 1:0;
+        }
+           }
+       }
         
         for(int i=0; i<row; i++){
             board[i] = temp[i].clone();
@@ -33,14 +26,11 @@ class Solution {
         
     }
     
-    
-    public int count(int [][]board, int i, int j){
+    public int count(int [][] board, int i, int j){
         
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] == 0) return 0;
         
-        if(i<0 || i>=board.length || j<0 || j>= board[0].length || board[i][j] == 0) return 0;
-        
-        
-        return 1;
+        return 1;        
     }
     
 }
