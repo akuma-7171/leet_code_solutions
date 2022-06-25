0038433b8d811bbe9dfa88ff38275d82509c761f
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-25 14:10:40 +0530

    Time: 184 ms (41.38%), Space: 42 MB (62.12%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,11 +1,17 @@
 class Solution {
-     boolean[][] visited;
+    
+    boolean visited[][];
+    
     public boolean exist(char[][] board, String word) {
-        visited = new boolean[board.length][board[0].length];
         
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if((word.charAt(0) == board[i][j]) && search(board, word, i, j, 0)){
+        int row = board.length;
+        int column = board[0].length;
+        
+        visited = new boolean[row][column];
+        
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column; j++){
+                if(board[i][j] == word.charAt(0) && search(board,i,j,0,word)){
                     return true;
                 }
             }
@@ -13,27 +19,34 @@ class Solution {
         
         return false;
     }
+                    
     
-    public boolean search(char[][]board, String word, int i, int j, int index){
-        
-        if(index == word.length()){
-            return true;
-        }
-        
-        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
-            return false;
-        }
-        
-        visited[i][j] = true;
-        if(search(board, word, i-1, j, index+1) || 
-           search(board, word, i+1, j, index+1) ||
-           search(board, word, i, j-1, index+1) || 
-           search(board, word, i, j+1, index+1)){
-            return true;
-        }
-        
-        visited[i][j] = false;
-        return false;
-    }
-    
+                   
+      public boolean search(char[][] board, int i, int j, int index, String word){
+          
+          if(index == word.length()){
+              return true;
+          }
+          
+          if(i<0 || i>=board.length || j<0 || j>= board[0].length || word.charAt(index) != board[i][j] || visited[i][j]){
+              return false;
+          }
+          
+          visited[i][j] = true;
+          
+          if(search(board,i+1,j,index+1,word)  ||
+             search(board,i,j+1,index+1, word) ||
+             search(board,i-1,j,index+1, word) ||
+             search(board,i,j-1,index+1,word))
+          {
+              return true;
+          }
+          
+          visited[i][j] = false;
+          
+          return false;
+          
+      }             
+                   
 }
+                   
