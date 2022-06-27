be4b4536bb3bb309b1983ad82b27ec99aee35dc2
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-27 11:16:09 +0530

    Time: 1 ms (19.89%), Space: 41.9 MB (19.42%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -13,8 +13,7 @@ class Solution {
             return Math.max(nums[0], nums[1]);
         }
         
-        
-        int dp[] = new int[nums.length];
+        int []dp = new int[nums.length];
         
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
@@ -22,7 +21,6 @@ class Solution {
         for(int i=2; i<nums.length; i++){
             dp[i] = Math.max(nums[i]+dp[i-2], dp[i-1]);
         }
-        
         
         return dp[nums.length-1];
         
