9beccdc6574d0d209934f1bc512949ef12b0cbaf
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-27 10:20:48 +0530

    Time: 5 ms (80.58%), Space: 42.2 MB (56.99%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -3,30 +3,34 @@ class Solution {
     int count = 0;
     
     public int countSubstrings(String s) {
+        
         if(s == null || s.length() == 0){
             return 0;
         }
         
         for(int i=0; i<s.length(); i++){
             
-           expand(s,i,i);
-           expand(s,i,i+1);
+            expand(s,i,i); // odd;
+            expand(s,i,i+1); // even;    
             
-        }
-        
-        return count;  
-    }
-    
-    public int expand(String s, int left, int right){
-        
-        while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
-            count++;
-            right++;
-            left--;
         }
         
         return count;
         
     }
+    
+    public int expand(String s , int left, int right){
+        
+        while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
+            count++;
+            left--;
+            right++;
+        }
+        
+        
+        return count;
+        
+    }
+    
     
 }
