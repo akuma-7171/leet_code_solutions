096ede2b28553735b17c5d8672679fd2fe415868
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-27 10:02:39 +0530

    Time: 17 ms (91.16%), Space: 45.5 MB (34.68%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -10,13 +10,12 @@ class Solution {
         for(int coin : coins){
             
             for(int i=coin; i<=amount; i++){
-                dp[i] = Math.min(dp[i], dp[i-coin]+1);
+                dp[i] = Math.min(dp[i],dp[i-coin]+1);
             }
             
         }
         
-        
-        return dp[amount] <= amount ? dp[amount] : -1;
+       return dp[amount] <= amount ? dp[amount] : -1;
         
     }
 }
