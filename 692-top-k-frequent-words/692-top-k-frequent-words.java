e1853f0cc794aa5628b77973a22b69b4039ac15a
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-06-28 15:44:35 +0530

    Time: 12 ms (31.10%), Space: 45.7 MB (13.23%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.java
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -6,7 +6,7 @@ class Solution {
             map.put(s, map.getOrDefault(s, 0)+ 1);
         
 		//custom comparator for comparing the words
-        Queue<String> q = new PriorityQueue<>((w1, w2) -> map.get(w1).equals(map.get(w2)) ?
+        PriorityQueue<String> q = new PriorityQueue<>((w1, w2) -> map.get(w1).equals(map.get(w2)) ?
                 w2.compareTo(w1) : map.get(w1) - map.get(w2));
         
 		//add to the heap
@@ -17,9 +17,12 @@ class Solution {
                                        
         //add to the result
         List<String> result = new ArrayList<>();
+        
         while (!q.isEmpty())
             result.add(q.poll());
+        
         Collections.reverse(result);
+        
         return result;
     }
 }
