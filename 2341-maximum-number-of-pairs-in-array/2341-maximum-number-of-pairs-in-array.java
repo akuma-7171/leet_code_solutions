dc469720a16fe902b70627db1836f00ac066f2b3
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-08-06 12:42:31 +0530

    Time: 9 ms (9.80%), Space: 43 MB (39.07%) - LeetHub

---
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] numberOfPairs(int[] nums) {
+        
+        if(nums.length == 1) return new int[]{0,1};
+        
+        int[] res = new int[2];
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i : nums){
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+        
+        System.out.println(map);
+        
+        for(int i : map.values()){
+            
+            if(i%2 == 0){
+                res[0] += (i)/2;
+            }
+            
+            if(i%2 != 0){
+                res[0] += (i-1)/2;
+            }
+            
+        }
+        
+        for(int i : map.keySet()){
+            if(map.get(i)%2 == 1){
+                res[1]++;
+            }
+        }
+        
+        return res;
+        
+    }
+}
