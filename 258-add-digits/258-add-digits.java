a15163b476f42cb2f79b35f60af1ce8e39531a7e
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-08-13 11:08:41 +0530

    Time: 2 ms (72.51%), Space: 41.5 MB (47.59%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,8 +1,10 @@
 class Solution {
    public int addDigits(int num) {
+       
    int sum = 0; 
    
     while ( num > 0 || sum > 9 ){
+        
         if (num == 0){
              num = sum ;
              sum = 0;
@@ -14,6 +16,7 @@ class Solution {
     }
     
     return sum;
+       
 }
     
 }
