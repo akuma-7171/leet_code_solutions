cfc02783240ba6d43d95be48ea91211b8f4aa633
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-09-15 19:18:45 +0530

    Time: 22 ms (91.75%), Space: 42.5 MB (91.02%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i: nums){
+            if(i%2 == 0) map.put(i, map.getOrDefault(i,0)+1);
+        }
+        
+        int max = Integer.MIN_VALUE;
+        
+        for(int i: map.values()){
+            max = Math.max(max, i);
+        }
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(int i : map.keySet()){
+            if(map.get(i) == max){
+                min = Math.min(min,i);
+            }
+        }
+        
+       
+        return min == Integer.MAX_VALUE ? -1 : min;
+        
+        
+    }
+}
