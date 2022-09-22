0ec8e89837cdec7e42c736995b04edbd1082384a
nihar dabhi <104818160+akuma-7171@users.noreply.github.com>
2022-09-22 18:16:18 +0530

    Time: 16 ms (20.81%), Space: 42.9 MB (79.66%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -15,13 +15,14 @@
  */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-    List<String> answer = new ArrayList<String>();
-    if (root != null) searchBT(root, "", answer);
-    return answer;
-}
-private void searchBT(TreeNode root, String path, List<String> answer) {
-    if (root.left == null && root.right == null) answer.add(path + root.val);
-    if (root.left != null) searchBT(root.left, path + root.val + "->", answer);
-    if (root.right != null) searchBT(root.right, path + root.val + "->", answer);
-  }
+        List<String> res = new ArrayList<>();
+        if(root != null) travel(root,"",res);
+        return  res;
+    }
+    
+    public void travel(TreeNode root, String s, List<String> res){
+        if(root.right == null && root.left == null) res.add(s+root.val);
+        if(root.left != null) travel(root.left,s+root.val+"->",res);
+        if(root.right != null) travel(root.right,s+root.val+"->",res);
+    }
 }
